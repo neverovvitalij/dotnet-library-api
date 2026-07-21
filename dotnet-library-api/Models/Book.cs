b5e8748135e9a2d90fab3bfde135af2375b92251
Vitalij Neverov <Vitalij.Neverov@training.lug-ag.de>
2026-07-21 11:11:58 +0200

    feat: add Author, Book, Genre, Loan entities

---
+++ dotnet-library-api/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace dotnet_library_api.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int PublishedYear { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; } = null!;
+        public ICollection<Genre> Genres { get; set; } = new List<Genre>();
+    }
+}
---
+++ dotnet-library-api/Models/Loan.cs
@@ -0,0 +1,13 @@
+﻿namespace dotnet_library_api.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; } = null!;
+        public string BottowerName { get; set; } = string.Empty;
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        
+    }
+}
